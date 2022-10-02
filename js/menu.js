1c1ae9ea63691d80d6fc63ea4185a7351626a266
Marianna <mariannaklos345@gmail.com>
2022-10-02 16:48:16 +0300

    adapting

---
+++ js/menu.js
@@ -0,0 +1,20 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+      menu: document.querySelector("[data-menu]"),
+    //  menuList: document.querySelector("[.mob__navigation-list]"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+//   refs.menuList.addEventListener("click", removeMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
+    }
+
+//     function removeMenu() {
+//     refs.menu.classList.add("is-hidden");
+//   }
+})();
